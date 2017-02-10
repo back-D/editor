4d055d18160bf38abed1e40e47c5c1ceae8bfec6
Nikita Kuzmin <backd80@gmail.com>
2017-02-10 16:31:54 +0200

    add row/col button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-let text, commaFlag, quoteFlag,
+let text, commaFlag, quoteFlag, val,
     textArea = document.getElementById('field'),
     commasBtn = document.getElementById('commas'),
-    quotesBtn = document.getElementById('quotes');
+    quotesBtn = document.getElementById('quotes'),
+    editBtn = document.getElementById('edit');
 
-// function addCommas() {
-//     textArea.value = textArea.value.split(' ').join(', ');
-// }
-//
-// function addQuotes() {
-//
-// }
+
+editBtn.addEventListener('click', function() {
+	val = textArea.value;
+	if (val.split(' ').length > 2) {
+		let arr = val.split(' ');
+		for (let i = 0; i < arr.length; i++) {
+			if (!arr[i]) {
+				arr.splice(i, 1);
+				--i;
+			}
+		}
+		textArea.value = arr.join('\n');
+	} else {
+		let arr = val.split('\n');
+		for (let i = 0; i < arr.length; i++) {
+			if (!arr[i]) {
+				arr.splice(i, 1);
+				--i;
+			}
+		}
+		textArea.value = arr.join(' ');
+	}
+}, false);
 
 
 commasBtn.addEventListener('click', function() {
@@ -17,10 +34,8 @@ commasBtn.addEventListener('click', function() {
         textArea.value = textArea.value.split(' ').join(', ');
         textArea.value = textArea.value.split('\n').join(', ');
 
-    } else {
-        alert('Already add commas');
+    	}
     }
 }, false);
 
 quotesBtn.addEventListener('click', function() {}, false);
-
