2eb1a2ad3d849064bbbf60a99b6007a5b757bc9b
Nikita Kuzmin <backd80@gmail.com>
2017-02-20 17:25:13 +0200

    -_-

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
-let text, commaFlag, quoteFlag, val,
+let val,
     textArea = document.getElementById('field'),
     commasBtn = document.getElementById('commas'),
     quotesBtn = document.getElementById('quotes'),
-    editBtn = document.getElementById('edit');
+    editBtn = document.getElementById('edit'),
+    flag = {
+        'comma': false,
+        'quote': false,
+        'row': false
+    };
 
+textArea.addEventListener('input', function() {
+    val = textArea.value;
+    if (val.split(',').length > 1) {
+        flag.comma = true;
+    } else if (val.split('"').length > 1) {
+        flag.quote = true;
+    } else if (val.split(' ').length > 1) {
+        flag.row = true;
+    } else {
+        for (let key in flag) {
+            key = false;
+        }
+    }
+}, false);
+
+function deleteCommas() {
+    val = textArea.value;
+    if (val.split(',').length > 1) {
+        textArea.value = val.split(',').join('');
+        flag.comma = false;
+    }
+}
+
+function addCommas() {
+    val = textArea.value;
+    if (flag.row) {
+        textArea.value = val.split(' ').join(', ');
+    } else {
+        textArea.value = val.split('\n').join(', ');
+    }
+    flag.comma = true;
+}
 
 editBtn.addEventListener('click', function() {
 	val = textArea.value;
@@ -16,6 +53,7 @@ editBtn.addEventListener('click', function() {
 			}
 		}
 		textArea.value = arr.join('\n');
+		flag.row = false;
 	} else {
 		let arr = val.split('\n');
 		for (let i = 0; i < arr.length; i++) {
@@ -25,17 +63,18 @@ editBtn.addEventListener('click', function() {
 			}
 		}
 		textArea.value = arr.join(' ');
+		flag.row = true;
 	}
 }, false);
 
 
 commasBtn.addEventListener('click', function() {
-    if (!commaFlag) {
-        textArea.value = textArea.value.split(' ').join(', ');
-        textArea.value = textArea.value.split('\n').join(', ');
-
-    	}
+    val = textArea.value;
+    deleteCommas();
+    if (flag.comma) {
+        addCommas();
     }
+
 }, false);
 
 quotesBtn.addEventListener('click', function() {}, false);
